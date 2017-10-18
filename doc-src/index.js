919cea578cde98a3bd67fdee0a52eaa3c040dbfd
Héctor Menéndez <hector@gik.mx>
2017-10-18 05:55:20 -0500

    Added files for doc-gen.

---
+++ doc-src/index.js
@@ -0,0 +1,25 @@
+import Thrower from '@gik/tools-thrower';
+import Checker from '@gik/tools-checker';
+import Logger from '@gik/tools-logger';
+import Streamer from '@gik/tools-streamer';
+import Populator from '@gik/tools-populator';
+import Mapper from '@gik/tools-mapper';
+
+/**
+ * An utility belt for the GIK development experience.
+ * @namespace Tools
+ */
+export default {
+    thrower: Thrower,
+    checker: Checker,
+    logger: Logger,
+    streamer: Streamer,
+    populator: Populator,
+    mapper: Mapper,
+};
+
+
+/**
+ * These are the available types for the tools.
+ * @namespace Types
+ */
